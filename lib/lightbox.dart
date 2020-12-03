f999987ec5ed05c3cc889a7bf9ddf0e1f6a7ba09
SherlockHolmes2045 <lemovou@gmail.com>
2020-12-04 00:05:08 +0100

    added code for the widget and route

--- lib/lightbox.dart
+++ lib/lightbox.dart
@@ -1,7 +1,174 @@
 library lightbox;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class LightBox extends StatefulWidget {
+
+  final List<String> images;
+  final int initialIndex;
+  final bool url;
+
+  LightBox(
+      this.images,
+      {
+        this.initialIndex = 0,
+        this.url = false
+      });
+  @override
+  _LightBoxState createState() => _LightBoxState();
+}
+
+class _LightBoxState extends State<LightBox> {
+
+  PageController controller ;
+  var currentPageValue = 0.0;
+  var mItemCount = 10;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    controller = PageController(viewportFraction: 1, keepPage: true, initialPage: widget.initialIndex);
+  }
+
+  void changePageViewPosition(int whichPage) {
+    if(controller != null){
+      if(whichPage < widget.images.length && whichPage >= 0){
+        controller.jumpToPage(whichPage);
+      }
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 2.5,
+              sigmaY: 2.5,
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: (MediaQuery.of(context).size.height / 100) * 8,
+                ),
+                GestureDetector(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                            right: (MediaQuery.of(context).size.width / 100),
+                            bottom: (MediaQuery.of(context).size.height / 100)),
+                        child: Icon(
+                          Icons.close,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            right:
+                            (MediaQuery.of(context).size.width / 100) * 16,
+                            bottom: (MediaQuery.of(context).size.height / 100)),
+                        child: Text(
+                          "Close",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              decoration: TextDecoration.none),
+                        ),
+                      )
+                    ],
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Container(
+                    height: (MediaQuery.of(context).size.height / 100) * 50,
+                    width: (MediaQuery.of(context).size.width / 100) * 70,
+                    child: PageView.builder(
+                        controller: controller,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: widget.images.length,
+                        itemBuilder: (context,position){
+                          return   Image.asset(
+                            widget.images[position],
+                            fit: BoxFit.cover,
+                          );
+                        }
+                    )
+                ),
+                Row(
+                  children: [
+                    GestureDetector(
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: (MediaQuery.of(context).size.width / 100) * 14, top: (MediaQuery.of(context).size.height / 100) * 1.5),
+                            child: Icon(
+                                Icons.arrow_back_ios
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only( top: (MediaQuery.of(context).size.height / 100) * 1.5),
+                            child: Text(
+                              'Prev',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  decoration: TextDecoration.none,
+                                  fontSize: 20.0
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      onTap: (){
+                        changePageViewPosition((controller.page-1).round());
+                      },
+                    ),
+                    GestureDetector(
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left:(MediaQuery.of(context).size.width / 100) * 42,top: (MediaQuery.of(context).size.height / 100) * 1.5),
+                            child: Row(
+                              children: [
+                                Text(
+                                  'Next',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      decoration: TextDecoration.none,
+                                      fontSize: 20.0
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.arrow_forward_ios_sharp,
+                                  color: Colors.black,
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      onTap: (){
+                        changePageViewPosition((controller.page+1).round());
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
---
+++ lib/lightbox_route.dart
@@ -0,0 +1,60 @@
+library lightbox;
+
+import 'package:flutter/material.dart';
+
+class LightBoxRoute extends PopupRoute {
+  LightBoxRoute({
+    @required this.builder,
+    this.dismissible = true,
+    this.label,
+    this.color,
+    RouteSettings setting,
+  }) : super(settings: setting);
+
+  final WidgetBuilder builder;
+  final bool dismissible;
+  final String label;
+  final Color color;
+
+  @override
+  Color get barrierColor => color;
+
+  @override
+  bool get barrierDismissible => dismissible;
+
+  @override
+  String get barrierLabel => label;
+
+  @override
+  Widget buildPage(
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      ) {
+    return builder(context);
+  }
+
+  @override
+  Widget buildTransitions(
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child,
+      ) {
+    return ScaleTransition(
+      scale: Tween<double>(
+        begin: 0.0,
+        end: 1.0,
+      ).animate(
+        CurvedAnimation(
+          parent: animation,
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
+      child: child,
+    );
+  }
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 400);
+}
